1522e64abe3bfff28a27e4640ebee5d64d4735be
Christine Lam <christinelam320@gmail.com>
2019-08-07 23:16:27 -0400

    updated js

--- client.js
+++ client.js
@@ -1,37 +1,32 @@
 $(function() {
   
-  // Called function to update the name, happiness, and weight of our pet in our HTML
-  checkAndUpdatePetInfoInHtml();
-
-  // When each button is clicked, it will "call" function for that button (functions are below)
-  $('.treat-button').click(clickedTreatButton);
-  $('.play-button').click(clickedPlayButton);
-  $('.exercise-button').click(clickedExerciseButton);
-  
-})
-
-  // Add a variable "pet_info" equal to a dictionary with the name (string), weight (number), and happiness (number) of your pet
   let pet_info = {
     name:'Summer',
     weight: 15,
     happiness: 88
   };
-
+  
+  checkAndUpdatePetInfoInHtml();
+  
+  $('.treat-button').click(clickedTreatButton);
+  $('.play-button').click(clickedPlayButton);
+  $('.exercise-button').click(clickedExerciseButton);
+  
   function clickedTreatButton() {
-    // Increase pet happiness
-    // Increase pet weight
+    pet_info['happiness'] = pet_info['happiness'] + 1;
+    pet_info['weight'] = pet_info['weight'] + 2;
     checkAndUpdatePetInfoInHtml();
   }
   
   function clickedPlayButton() {
-    // Increase pet happiness
-    // Decrease pet weight
+    pet_info['happiness'] = pet_info['happiness'] + 2;
+    pet_info['weight'] = pet_info['weight'] - 1;
     checkAndUpdatePetInfoInHtml();
   }
   
   function clickedExerciseButton() {
-    // Decrease pet happiness
-    // Decrease pet weight
+    pet_info['happiness'] = pet_info['happiness'] - 1;
+    pet_info['weight'] = pet_info['weight'] - 1;
     checkAndUpdatePetInfoInHtml();
   }
 
@@ -41,12 +36,15 @@ $(function() {
   }
   
   function checkWeightAndHappinessBeforeUpdating() {
-    // Add conditional so if weight is lower than zero, set it back to zero
+    if (pet_info['weight'] < 1) {
+      pet_info['weight'] = 1;
+    }
   }
   
-  // Updates your HTML with the current values in your pet_info dictionary
   function updatePetInfoInHtml() {
     $('.name').text(pet_info['name']);
     $('.weight').text(pet_info['weight']);
     $('.happiness').text(pet_info['happiness']);
   }
+  
+})
